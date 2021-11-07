027ffc20fd44429b119380bb3a1f0d0791f2c86d
Marcelo de Castro Cardoso <marcelo.cardoso@cuia.com.br>
2021-11-06 21:52:43 -0300

    first commit

---
+++ chat.py
@@ -0,0 +1,97 @@
+import json
+import configparser
+from flask import Flask, render_template
+from flask import request
+from ibm_watson import AssistantV2
+from ibm_watson import ApiException
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
+config = configparser.RawConfigParser()
+config.read('conf/config.properties')
+
+assistant = None
+
+## WATSON ASSISTANT AUTENTICATION
+def watson_assistant():
+    global assistant
+
+    api_key = config.get('ibm-cloud', 'ASSISTANT_IAM_APIKEY')
+    assistant_url = config.get('environment', 'ASSISTANT_URL')
+    version = config.get('environment', 'VERSION')
+    without_ssl = bool(config.get('conn-params', 'DISABLE_SSL_VERIFICATION'))
+
+    authenticator = IAMAuthenticator(api_key)
+    assistant = AssistantV2(
+        version=version,
+        authenticator=authenticator
+    )
+
+    assistant.set_service_url(assistant_url)
+    assistant.set_disable_ssl_verification(without_ssl)
+
+## INTANTIATE FLASK AND WATSON ASSISTANT
+app = Flask(__name__)
+watson_assistant()
+
+## LOAD FRONTEND
+@app.route('/')
+def index():
+    return render_template('index-chat.html')
+
+## CREATE WATSON SESSION ROUTE
+@app.route('/api/session')
+def session_start():
+    try:
+        detailed_response = assistant.create_session(
+            assistant_id=config.get('environment', 'ASSISTANT_ID')
+        )
+
+        return str(detailed_response)
+    except ApiException as ex:
+        return json.dumps({
+          'output': {
+            'text': 'Method failed with status code ' + str(ex.code) + ': ' + ex.message
+          },
+        })
+
+## SEND MESSAGE FROM WATSON ROUTE
+@app.route('/api/message', methods=['POST'])
+def send_message():
+    try:
+        json_body = request.get_json(force=True)
+
+        detailed_response = assistant.message(
+                assistant_id=config.get('environment', 'ASSISTANT_ID'),
+                session_id=json_body['session_id'],
+                input={
+                    'message_type': 'text',
+                    'text': json_body['input']['text']
+                }
+            )
+
+        return str(detailed_response)
+    except ApiException as ex:
+        return json.dumps({
+          'output': {
+            'text': 'Method failed with status code ' + str(ex.code) + ': ' + ex.message
+          },
+        })
+
+## DESTROY WATSON SESSION ROUTE
+@app.route('/api/interrupt', methods=['POST'])
+def session_stop():
+    try:
+        json_body = request.get_json(force=True)
+
+        detailed_response = assistant.delete_session(
+                assistant_id=config.get('environment', 'ASSISTANT_ID'),
+                session_id=json_body['session_id']
+            )
+
+        return str(detailed_response)
+    except ApiException as ex:
+        return json.dumps({
+          'output': {
+            'text': 'Method failed with status code ' + str(ex.code) + ': ' + ex.message
+          },
+        })
---
+++ hello.py
@@ -0,0 +1,96 @@
+import json
+import configparser
+from flask import Flask, render_template
+from ibm_watson import AssistantV2
+from ibm_watson import ApiException
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
+config = configparser.RawConfigParser()
+config.read('conf/config.properties')
+
+session_id = ''
+assistant = None
+
+## WATSON ASSISTANT AUTENTICATION
+def watson_assistant():
+    global assistant
+
+    api_key = config.get('ibm-cloud', 'ASSISTANT_IAM_APIKEY')
+    assistant_url = config.get('environment', 'ASSISTANT_URL')
+    version = config.get('environment', 'VERSION')
+    without_ssl = bool(config.get('conn-params', 'DISABLE_SSL_VERIFICATION'))
+
+    authenticator = IAMAuthenticator(api_key)
+    assistant = AssistantV2(
+        version=version,
+        authenticator=authenticator
+    )
+
+    assistant.set_service_url(assistant_url)
+    assistant.set_disable_ssl_verification(without_ssl)
+
+## INTANTIATE FLASK AND WATSON ASSISTANT
+app = Flask(__name__)
+watson_assistant()
+
+## LOAD FRONTEND
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+## CREATE WATSON SESSION ROUTE
+@app.route('/start')
+def start():
+    global session_id
+
+    try:
+        if not session_id:
+            response = assistant.create_session(
+                assistant_id=config.get('environment', 'ASSISTANT_ID')
+            ).get_result()
+
+            session_id = response['session_id']
+
+            return json.dumps(response, indent=2)
+        else:
+            return "Session has already started: " + str(session_id)
+    except ApiException as ex:
+        return "Method failed with status code " + str(ex.code) + ": " + ex.message
+
+## DESTROY WATSON SESSION ROUTE
+@app.route('/stop')
+def stop():
+    global session_id
+    try:
+        if session_id:
+            response = assistant.delete_session(
+                assistant_id=config.get('environment', 'ASSISTANT_ID'),
+                session_id=session_id
+            ).get_result()
+
+            session_id = ''
+            return json.dumps(response, indent=2) 
+        else:
+            return "Session not started."
+    except ApiException as ex:
+        return "Method failed with status code " + str(ex.code) + ": " + ex.message
+
+## SEND MESSAGE FROM WATSON ROUTE
+@app.route('/talk/<string:text>')
+def talk(text):
+    try:
+        if session_id:
+            response = assistant.message(
+                assistant_id=config.get('environment', 'ASSISTANT_ID'),
+                session_id=session_id,
+                input={
+                    'message_type': 'text',
+                    'text': text
+                }
+            ).get_result()
+
+            return json.dumps(response, indent=2)
+        else:
+            return "Session not started."
+    except ApiException as ex:
+        return "Method failed with status code " + str(ex.code) + ": " + ex.message
